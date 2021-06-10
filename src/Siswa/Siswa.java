fa836090c918faf4b48479f8c7e2e3df35b8d166
HazielC <haziel_justice_29rpl@student.smktelkom-mlg.sch.id>
2021-06-10 08:56:19 +0700

    hore berhasil

---
+++ src/Siswa/Siswa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Siswa;
+
+
+public class Siswa {
+public static void murid1 (){
+System.out.println("Budi");
+}
+public static void murid2 (){
+System.out.println("Lina");
+}
+public static void murid3 (){
+System.out.println("Siti");
+}
+public static void murid4 (){
+System.out.println("Jojo");
+}
+public static void alamat1 (){
+System.out.println("Jln. Danau Sentani");
+}
+public static void alamat2 (){
+System.out.println("Jln. S Supriadi");
+}
+public static void alamat3 (){
+System.out.println("Jln. Janti Barat");
+}
+public static void alamat4 (){
+System.out.println("Jln. Danau Toba");
+}
+
+public static void main(String[] args) {
+    
+System.out.println ("===== DATA ALAMAT SISWA =====");
+System.out.print ("No Absen 1 : ");
+murid1();
+System.out.print ("Alamat Rumah : ");
+alamat1();
+System.out.print ("No Absen 2 : ");
+murid2();
+System.out.print ("Alamat Rumah : ");
+alamat2();
+System.out.print ("No Absen 3 : ");
+murid3();
+System.out.print ("Alamat Rumah : ");
+alamat3();
+System.out.print ("No Absen 4 : ");
+murid4();
+System.out.print ("Alamat Rumah : ");
+alamat4();
+
+    }
+}
+    
+
